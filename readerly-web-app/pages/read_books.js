427dfa379a7c86b8110d6b8e9db58279fa6f3d9c
Nahom Habtamu <nhabtamu42@gmail.com>
2023-04-20 03:58:07 +0300

    get read books from db

--- readerly-web-app/pages/read_books.js
+++ readerly-web-app/pages/read_books.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Box, Typography } from "@mui/material";
 import Book from "@/components/book";
+import { useAuth } from "@clerk/nextjs";
+import { supabaseClient } from "@/utils/supabaseClient";
 
-export default function ReadBooks () {
+export default function Wishlists() {
+
+    const [readBooks, setReadBooks] = useState([]);
+    const { userId, getToken } = useAuth();
+
+    useEffect(() => {
+        const getReadBooks = async () => {
+            const token = await getToken({ template: 'supabase' })
+            const supabase = await supabaseClient(token);
+            const { data, error } = await supabase
+                .from('wishlist')
+                .select('*')
+                .eq('User-ID', userId)
+            setReadBooks(data);
+        }
+        getReadBooks();
+    }, [])
+
     return (
         <Box sx={{ width: '100%', marginTop: 10, height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-            <Typography sx={{ textAlign: 'center' }} variant="h4">Books you read</Typography>
-            <Box sx={{ display: 'flex', padding: 10 }}>
-                <Book />
-                <Book />
-                <Book />
-                <Book />
+            <Typography sx={{ textAlign: 'center' }} variant="h4">Books you have read</Typography>
+            <Box sx={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr', padding: 10 }}>
+                {readBooks.map((book, index) => {
+                    { console.log('first: ', book) }
+                    return <Book key={book.id} type='read' book={book} />
+                })}
             </Box>
         </Box>
     )
