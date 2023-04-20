590d1685b4cc91fc1707b5afefadf09c2c874596
Nahom Habtamu <nhabtamu42@gmail.com>
2023-04-20 03:57:34 +0300

    add to read & wishlist feature

--- readerly-web-app/components/book.js
+++ readerly-web-app/components/book.js
@@ -5,33 +5,67 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 
-export default function Book() {
+import { useAuth } from '@clerk/nextjs';
+import { supabaseClient } from '@/utils/supabaseClient';
+
+export default function Book({ book, type }) {
+    const { userId, getToken } = useAuth();
+
+    { console.log('inside book: ', book) }
+
+    const addToWishlist = async () => {
+        const token = await getToken({ template: 'supabase' })
+        const supabase = await supabaseClient(token);
+        const { data, error } = await supabase
+            .from('wishlist')
+            .insert(
+                { ...book, 'User-ID': userId }
+            )
+            .single()
+    }
+
+    const addToReadBooks = async () => {
+        const token = await getToken({ template: 'supabase' })
+        const supabase = await supabaseClient(token);
+        const { data, error } = await supabase
+            .from('read_books')
+            .insert(
+                { ...book, 'User-ID': userId }
+            )
+            .single()
+
+        console.log('first: ', data, error)
+    }
+
     return (
         <Card sx={{ maxWidth: 345, marginX: 1 }}>
             <CardActionArea>
                 <CardMedia
                     component="img"
                     height="300"
-                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuU66akzUklOuCKb_SdK9Dz1f6Gsnh3CxOSKt39riYKzn2Fau7"
+                    image={book['image-url']}
                     alt="green iguana"
                 />
                 <CardContent>
                     <Typography gutterBottom component="div" sx={{ fontsiz: 15, fontWeight: 'bold' }}>
-                        Structures : Or Why Things Don't Fall Down
+                        {/* Structures : Or Why Things Don't Fall Down */}
+                        {book['book-title']}
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                        J.E. Gordon
+                        {/* J.E. Gordon */}
+                        {book.author}
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                        1986
+                        {/* 1986 */}
+                        {book.year}
                     </Typography>
                 </CardContent>
             </CardActionArea>
-            <CardActions sx={{display: 'flex', justifyContent: 'space-between'}}>
-                <Button sx={{backgroundColor: '#6fb554'}} size="small" color="primary" variant='contained'>
+            <CardActions sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Button disabled={type=='read'} sx={{ backgroundColor: '#6fb554' }} size="small" color="primary" variant='contained' onClick={addToReadBooks}>
                     Read
                 </Button>
-                <Button size="small" color="primary" variant='contained'>
+                <Button disabled={type=='wishlist' || type=='read'} size="small" color="primary" variant='contained' onClick={addToWishlist}>
                     Wishlist
                 </Button>
             </CardActions>
